790075c173f396c8f3e49e5e379a53c76e323518
Antonio Mika <antoniomika@gmail.com>
2017-03-27 22:38:00 -0400

    Added Docker things

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # unchained
 
-## Getting started:
-1. Get yarn
-2. Run yarn install
-3. Run yarn start
-4. Profit?
+## Installation (soon[TM])
+1. [Get Docker](https://docs.docker.com/engine/getstarted/step_one/#/step-1-get-docker)
+2. [Get Docker Compose](https://docs.docker.com/compose/install/)
+4. Clone this repo: `git clone https://github.com/mhacks/unchained`
+4. Change directory to the deploy repo: `cd unchained/deploy/`
+5. Start whatever environment you want
+    - Development
+        - `docker-compose -f development.yml up -d`
+        - **NOTE: Your git repo will be linked to the development environment, so your local changes will be reflected with a container restart**
+    - Production (You're gonna need some more env data)
+        - `docker-compose -f production.yml up -d`
+        - **NOTE: This takes care of setting up NGINX AND LetsEncrypt with the appropriate hosts (and autorenewal!).**
+6. Access `http://localhost:3000` and start developing!
--- src/app.js
+++ src/app.js
@@ -2,27 +2,9 @@ var http = require('http'),
     morgan = require('morgan'),
     express = require('express'),
     app = express(),
-    swig = require('swig'),
     server = http.createServer(app),
     session = require('express-session'),
     bodyParser = require('body-parser');
-
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
-
-app.engine('html', swig.renderFile);
-app.set('view engine', 'html');
-app.set('views', './views');
-app.set('view cache', false);
-
-app.use(express.static('public'));
-app.use(session({
-    secret: process.env.SECRET || 'mhacks',
-    resave: true,
-    saveUninitialized: true
-}));
 
 morgan.token('remote-addr', function(req, res) {
     return req.headers['x-forwarded-for'] || req.ip;
@@ -30,8 +12,27 @@ morgan.token('remote-addr', function(req, res) {
 
 app.use(morgan("combined"));
 
-swig.setDefaults({
-    cache: false
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.set('view engine', 'pug');
+app.set('views', './src/views');
+app.set('view cache', false);
+
+app.use(express.static('./src/public'));
+app.use(session({
+    secret: process.env.SECRET || 'mhacks',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.get('/', function(req, res) {
+    res.render('index', {
+        title: 'Hello,',
+        message: 'World!'
+    });
 });
 
 server.listen(process.env.PORT || 3000);
