1bdef26e19d52eadd0174988ba82bb684c47fb59
Connor Krupp <kruppcon@umich.edu>
2017-05-06 20:26:06 -0400

    Fuck git

---
+++ app/pages/blackout/hero.jsx
@@ -0,0 +1,142 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { devices } from '../../styles';
+import { TextSubmit, Container } from '../../components';
+import { SubscribeThunks } from '../../actions';
+import { routes } from '../../constants';
+import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
+
+const VideoPlaceholderImage = require('../../../static/blackout/video-placeholder.png');
+
+/*
+const HeaderWrapper = styled.div`
+    display: flex;
+    height: 90px;
+    alignItems: center;
+    justifyContent: center;
+`;
+
+const HeaderLogoWrapper = styled.div`
+    display: flex;
+    alignItems: center;
+    height: 90%;
+`;
+*/
+
+const HeroContainer = styled.div`
+    overflow: auto;
+    display: flex;
+    flexDirection: column;
+
+    ${devices.desktop`
+        flexDirection: row;
+        alignItems: center;
+    `}
+`;
+
+const LeftSideContainer = styled.div`
+    ${devices.desktop`
+        width: calc(40% - 20px);
+        float: left;
+    `}
+`;
+
+const VideoPlaceholderContainer = styled.div`
+    ${devices.desktop`
+        width: 60%;
+        float: right;
+    `}
+`;
+
+const VideoPlaceholder = styled.img`
+    width: 100%;
+    height: auto;
+`;
+
+const TextSubmitPositioner = styled.div`
+    marginBottom: 20px;
+    textAlign: center;
+    ${devices.desktop`
+        position: absolute;
+        margin: 0;
+    `}
+`;
+
+const Text = styled.h2`
+    fontSize: 22px;
+    marginBottom: 0;
+    textAlign: center;
+    ${devices.desktop`
+        fontSize: 28px;
+        margin: -50px 15px 25px 0;
+        textAlign: left;
+    `}
+`;
+
+class BlackoutHero extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            email: '',
+            feedback: ''
+        };
+
+        this.emailChange = this.emailChange.bind(this);
+        this.submitEmail = this.submitEmail.bind(this);
+    }
+
+    componentDidMount() {
+        this.props.dispatch(push(routes.SUBSCRIBE));
+    }
+
+    emailChange(e) {
+        this.setState({
+            email: e.target.value
+        });
+    }
+
+    submitEmail() {
+        this.props.dispatch(SubscribeThunks.subscribe(this.state.email));
+        this.setState({ email: '' });
+    }
+
+    render() {
+        return (
+            <Container>
+                <HeroContainer>
+                    <LeftSideContainer>
+                        <Text>MHacks helps you turn your dreams into reality.</Text>
+                        <TextSubmitPositioner>
+                            <TextSubmit
+                                placeholder="your@email.com"
+                                buttonText="Subscribe"
+                                focusColor="#350044"
+                                baseColor="#E6E6E6"
+                                feedback={this.props.status.message}
+                                value={this.state.email}
+                                onChange={e => {
+                                    this.emailChange(e);
+                                }}
+                                onSubmit={this.submitEmail}
+                            />
+                        </TextSubmitPositioner>
+                    </LeftSideContainer>
+                    <VideoPlaceholderContainer>
+                        <VideoPlaceholder src={VideoPlaceholderImage} />
+                    </VideoPlaceholderContainer>
+                </HeroContainer>
+            </Container>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        status: state.subscribeState
+    };
+}
+
+export default connect(mapStateToProps)(BlackoutHero);
--- app/pages/blackout/index.jsx
+++ app/pages/blackout/index.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { push } from 'react-router-redux';
 import styled from 'styled-components';
 
-import { routes } from '../../constants';
 import { devices } from '../../styles';
-import { SubscribeThunks } from '../../actions';
-import { InputText } from '../../components';
 import Header from './header.jsx';
+import Hero from './hero.jsx';
 
 /* Containers */
 const Page = styled.div`
@@ -70,54 +66,14 @@ const Right = styled.div`
 
 /* Page Component */
 class BlackoutPage extends React.Component {
-    constructor() {
-        super();
-
-        this.state = {
-            email: '',
-            feedback: ''
-        };
-
-        this.emailChange = this.emailChange.bind(this);
-        this.submitEmail = this.submitEmail.bind(this);
-    }
-
-    componentDidMount() {
-        this.props.dispatch(push(routes.SUBSCRIBE));
-    }
-
-    emailChange(e) {
-        this.setState({
-            email: e.target.value
-        });
-    }
-
-    submitEmail() {
-        this.props.dispatch(SubscribeThunks.subscribe(this.state.email));
-        this.setState({ email: '' });
-    }
-
     render() {
         return (
             <Page>
                 <Header />
+                <Hero />
                 <FlexBox>
                     <VerticalCenter>
                         <Subhead>Subscribe for Updates</Subhead>
-                        <InputText
-                            color={this.props.theme.darkPink}
-                            borderColor="white"
-                            placeholder="ENTER YOUR EMAIL FOR UPDATES"
-                            placeholderColor={this.props.theme.darkPink}
-                            feedbackColor="white"
-                            width="300px"
-                            feedback={this.props.status.message}
-                            value={this.state.email}
-                            onChange={e => {
-                                this.emailChange(e);
-                            }}
-                            onSubmit={this.submitEmail}
-                        />
                     </VerticalCenter>
                 </FlexBox>
                 <Footer>
