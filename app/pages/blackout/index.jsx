9e0f8dcba8f1a0b36de69d052483469880d29988
Robert Allen <robby.allen24@gmail.com>
2017-05-08 22:32:55 -0400

    cleaned up the blackout page. Created space for FAQ section (#53)

--- app/pages/blackout/index.jsx
+++ app/pages/blackout/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { devices } from '../../styles';
 import Header from './header.jsx';
 import Hero from './hero.jsx';
+import Faq from './faq.jsx';
 
 /* Containers */
 const Page = styled.div`
@@ -13,60 +13,12 @@ const Page = styled.div`
     color: #F1F7FF;
 `;
 
-const FlexBox = styled.div`
-    display: flex;
-
-    ${devices.tablet`
-        height: 90%;
-    `}
-`;
-
-const VerticalCenter = styled.div`
-    alignSelf: center;
-    margin: auto;
-    display: block;
-    position: relative;
-    paddingBottom: 30px;
-    textAlign: center;
-`;
-
 /* Styled elements */
-const Subhead = styled.h3`
-    marginBottom: 30px;
-    fontSize: 20px;
-`;
-
-const Anchor = styled.a`
-    marginLeft: 10px;
-    color: #BFBFBF;
-`;
-
-/* Footer Section */
-const Footer = styled.div`
-    display: flex;
-    flexWrap: wrap;
-    justifyContent: space-between;
-    alignItems: flex-end;
-    height: 10%;
-`;
-
-const Left = styled.div`
-    ${devices.tablet`
-        marginLeft: 10px;
-        marginRight: 10px;
-    `}
-`;
-
-const Right = styled.div`
-    ${devices.tablet`
-        marginLeft: 10px;
-        marginRight: 10px;
-    `}
-`;
-
-const HeroWrapper = styled.div`
+const StyledHero = styled(Hero)`
     margin: 35px 0;
 `;
+
+const StyledFaq = styled(Faq)``;
 
 /* Page Component */
 class BlackoutPage extends React.Component {
@@ -74,28 +26,8 @@ class BlackoutPage extends React.Component {
         return (
             <Page>
                 <Header />
-                <HeroWrapper>
-                    <Hero />
-                </HeroWrapper>
-                <FlexBox>
-                    <VerticalCenter>
-                        <Subhead>Subscribe for Updates</Subhead>
-                    </VerticalCenter>
-                </FlexBox>
-                <Footer>
-                    <Left>
-                        Have any questions?
-                        <Anchor href="mailto:hackathon@umich.edu">
-                            hackathon@umich.edu
-                        </Anchor>
-                    </Left>
-                    <Right>
-                        Sponsorship inquiries:
-                        <Anchor href="mailto:sponsor-mhacks@umich.edu">
-                            sponsor-mhacks@umich.edu
-                        </Anchor>
-                    </Right>
-                </Footer>
+                <StyledHero />
+                <StyledFaq />
             </Page>
         );
     }
