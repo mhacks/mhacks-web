e2d23e3033ec9e6820a85d4b92b1ef958d55effb
Andrew Boland <andrewbo@umich.edu>
2017-05-08 22:13:50 -0400

    Added section components and fixed some bugs

---
+++ app/pages/blackout/section_components.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const SectionHeader = styled.h2`
+    fontSize: 50px;
+    textTransform: uppercase;
+    color: ${props => props.theme.teal};
+    textShadow: 0px 0px 50px ${props => props.theme.teal};
+`;
+
+const SectionBody = styled.p`
+    color: white;
+    fontSize: 15px;
+    paddingLeft: 100px;
+`;
+
+export { SectionHeader, SectionBody };
--- app/styles/theme.js
+++ app/styles/theme.js
@@ -1,6 +1,6 @@
 // example use
 export default {
-    primaryFont: 'Avenir Next',
+    primaryFont: 'Lato',
     teal: '#1CA3A3',
     pink: '#FF2A8F',
     darkPink: '#CA2171'
