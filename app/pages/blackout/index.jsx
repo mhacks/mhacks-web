e9dfc0fe4735b0265a1e3413ceb14c7d03c23bfe
Jeffrey Chang <changjef@umich.edu>
2017-05-11 13:02:19 -0400

    styled hero

--- app/pages/blackout/index.jsx
+++ app/pages/blackout/index.jsx
@@ -6,25 +6,14 @@ import Header from './header.jsx';
 import Hero from './hero.jsx';
 
 import Faq from './faq.jsx';
-import QuoteSection from './QuoteSection/index';
+import QuoteSection from './QuoteSection';
 
 /* Containers */
 const Page = styled.div`
+    backgroundColor: black;
     width: 100%;
+    height: 100%;
     color: #F1F7FF;
-`;
-
-const FlexBox = styled.div`
-    display: flex;
-`;
-
-const VerticalCenter = styled.div`
-    alignSelf: center;
-    margin: auto;
-    display: block;
-    position: relative;
-    paddingBottom: 30px;
-    textAlign: center;
 `;
 
 /* Styled elements */
@@ -36,44 +25,20 @@ const StyledFaq = styled(Faq)``;
 
 /* Page Component */
 class BlackoutPage extends React.Component {
-    
-    render() {
-        return (
-            <Page>
-                <Header />
-                <StyledHero />
-                <StyledFaq />
-                <HeroWrapper>
-                    <Hero />
-                </HeroWrapper>
-                <FlexBox>
-                    <VerticalCenter>
-                        <Subhead>Subscribe for Updates</Subhead>
-                    </VerticalCenter>
-                </FlexBox>
-            <QuoteSection />
-            <Footer>
-              <Left>
-                Have any questions?
-                <Anchor href="mailto:hackathon@umich.edu">
-                  hackathon@umich.edu
-                </Anchor>
-              </Left>
-              <Right>
-                Sponsorship inquiries:
-                <Anchor href="mailto:sponsor-mhacks@umich.edu">
-                  sponsor-mhacks@umich.edu
-                </Anchor>
-              </Right>
-            </Footer>
-
-            </Page>
-        );
-    }
+  render() {
+    return (
+      <Page>
+        <Header />
+        <StyledHero />
+        <StyledFaq />
+        <QuoteSection />
+      </Page>
+    );
+  }
 }
 
 function mapStateToProps(/*state*/) {
-    return {};
+  return {};
 }
 
 export default connect(mapStateToProps)(BlackoutPage);
