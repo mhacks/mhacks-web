c822dfbe5be4ab8c7d63f0623b65472a53c05bfd
Jeffrey Chang <changjef@umich.edu>
2017-05-11 20:26:09 -0400

    responsive design

--- app/pages/blackout/section_components.jsx
+++ app/pages/blackout/section_components.jsx
@@ -1,16 +1,24 @@
 import styled from 'styled-components';
+import { devices } from '../../styles';
 
 const SectionHeader = styled.h2`
     fontSize: 50px;
     textTransform: uppercase;
     color: ${props => props.theme.teal};
     textShadow: 0px 0px 50px ${props => props.theme.teal};
+    
 `;
 
 const SectionBody = styled.p`
     color: white;
-    fontSize: 2em;
-    paddingLeft: 100px;
+    fontSize: 1.8em;
+    paddingLeft: 15%;
+    paddingRight: 5%;
+    
+    ${devices.desktop`
+        font-size: 2.0em;
+        
+    `}
 `;
 
 const Links = styled.a`
--- app/pages/blackout/sponsorship.jsx
+++ app/pages/blackout/sponsorship.jsx
@@ -1,81 +1,89 @@
 import React from 'react';
 import styled from 'styled-components';
-import { SectionHeader, SectionBody, Links } from './section_components';
+import {SectionHeader, SectionBody, Links} from './section_components';
+import { devices } from '../../styles';
 import Orb from 'icons/orb_2.png';
 
 const Wrapper = styled.div`
-  display: flex;
-  width: 130%;
-  overflow: hidden;
-    
+        display: flex;
+        width: 115%;
+        overflow: hidden;
 `;
 
-const TextWrapper = styled.div`
-    display: flex;
-
-    
-`;
 const OrbWrapper = styled.div`
-    flexBasis: 50%;
-    flexGrow: 1;
-    
-  
-    
+        flexBasis: 0;
+        display: none;
+        flexGrow: 1;
+        ${devices.tablet`
+            flexBasis: 50%;
+            display: block;
+        `}
+        
 `;
+
 const OrbImage = styled.img`
-  display: flex;
-  flexDirection: column;
-  alignSelf: flex-start;
-  width: 100%;
-  height: auto;
-  
+        display: flex;
+        flexDirection: column;
+        alignSelf: flex-start;
+        width: 100%;
+        height: auto;
+        padding: 20px 0;
+
 `;
+
 const SectionWrapper = styled.div`
-  flexGrow: 1;
-  flexDirection: row;
-  flexBasis: 50%;
-  display: flex;
-  alignSelf: flex-end;
-
+        flexGrow: 1;
+        flexDirection: row;
+        flexBasis: 50%;
+        display: flex;
+        alignSelf: flex-end;
+        ${devices.tablet`
+             flexBasis: 50%;
+             display: block;
+        `}
 `;
+
 const Section = styled.section`
-    display: flex;
-    flexDirection: column;
-    flexBasis: 80%;
-    justifyContent: center;
-
+        display: flex;
+        flexDirection: column;
+        flexBasis: 70%;
+        justifyContent: center;
+        ${devices.tablet`
+             flexBasis: 90%;
+             display: block;
+        `}
 `;
+
 const CenterAlign = styled.div`
-  display: flex;
-  flexDirection: row;
-  justifyContent: center;
+        display: flex;
+        flexDirection: row;
+        justifyContent: center;
+        
 
 `;
 
 export default () => (
-  <div style={{overflow: 'hidden'}}>
-    <Wrapper>
-        <SectionWrapper>
-            <CenterAlign>
-              <Section>
-                <SectionHeader> Sponsorship </SectionHeader>
-                <SectionBody>
-                  MHacks is the gateway between you and the top tech talent in the world. Whether you come to network, promote brand awareness, or get feedback on a product or API, we'll provide the tools you need to make the most out of your experience.
-                </SectionBody>
-                <SectionBody>
-                  Interested in sponsoring? Want more information? Shoot us an email at <Links href="mailto:kevin@mhacks.org">kevin@mhacks.org</Links>
-                </SectionBody>
-              </Section>
-
-            </CenterAlign>
-        </SectionWrapper>
-
-      <OrbWrapper>
-        <OrbImage src={Orb}/>
-      </OrbWrapper>
-      <TextWrapper>
-
-      </TextWrapper>
-    </Wrapper>
-  </div>
+    <div style={{overflow: 'hidden'}}>
+        <Wrapper>
+            <SectionWrapper>
+                <CenterAlign>
+                    <Section>
+                        <SectionHeader> Sponsorship </SectionHeader>
+                        <SectionBody>
+                            MHacks is the gateway between you and the top tech talent in the world. Whether you come to network,
+                            promote brand awareness, or get feedback on a product or API, we'll provide the tools you need to make the
+                            most out of your experience.
+                        </SectionBody>
+                        <SectionBody>
+                            Interested in sponsoring? Want more information? Shoot us an email at <Links
+                            href="mailto:kevin@mhacks.org">kevin@mhacks.org</Links>
+                        </SectionBody>
+                    </Section>
+                </CenterAlign>
+            </SectionWrapper>
+            <OrbWrapper>
+                <OrbImage src={Orb}/>
+            </OrbWrapper>
+        </Wrapper>
+    </div>
 );
