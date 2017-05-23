58c00c1f712cf2a98522a63fd817d4938c1c19fd
Andrew Boland <andrewbo@umich.edu>
2017-05-22 20:44:01 -0400

    Started announcements schema work

--- server/db/model/Announcement.js
+++ server/db/model/Announcement.js
@@ -2,3 +2,44 @@
 var mongoose = require('../index.js'),
     config = require('../../../config/default.js'),
     Schema = mongoose.Schema;
+
+// Define the document Schema
+var schema = new mongoose.Schema({
+    title: String,
+    body: {
+        type: String,
+        default: ''
+    },
+    //Indexed?
+    broadcastAt: {
+        type: Date,
+        default: Date.now,
+        index: true
+    },
+    category: {
+        type: String,
+        enum: ['Emergency', 'Logistics', 'Food', 'Event', 'Sponsored']
+    },
+    approved: {
+        type: Boolean,
+        default: false
+    },
+    sent: {
+        type: Boolean,
+        default: false
+    }
+});
+
+// Allow us to query by title
+schema.query.byTitle = function(title) {
+    return this.findOne({
+        title: new RegExp(title, 'i')
+    });
+};
+
+// Allow us to query by token
+schema.query.byCategory = function(category) {
+    return this.findOne({
+        category: category
+    });
+};
