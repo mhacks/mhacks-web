9ba34f9daf1ebbdaa94af2d35770fc24d4829f54
Andrew Boland <andrewbo@umich.edu>
2017-05-29 16:40:21 -0400

    Started work on the route

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,11 +2,13 @@ var router = require('express').Router(),
     authHandler = require('./api/auth.js'),
     emailHandler = require('./api/email.js'),
     userHandler = require('./api/user.js'),
+    announcementHandler = require('./api/announcement.js'),
     authMiddleware = require('../middleware/auth.js');
 
 router.use('/auth', authHandler);
 router.use('/email', emailHandler);
 router.use('/user', authMiddleware('api'), userHandler);
+router.use('/announcements', announcementHandler);
 
 router.get('/', function(req, res) {
     res.send('API');
