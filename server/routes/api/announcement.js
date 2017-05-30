97891804f824f868506698116f1e84e63f0a93a9
Andrew Boland <andrewbo@umich.edu>
2017-05-29 21:14:38 -0400

    More routes work

--- server/routes/api/announcement.js
+++ server/routes/api/announcement.js
@@ -2,18 +2,32 @@ var router = require('express').Router(),
     Announcements = require('../../db/model/Announcement.js');
 
 router.get('/', function(req, res) {
-    if (req.session.loggedIn && req.session.can_edit_announcement) {}
-    else {}
-    Announcements.find()
-        .byBroadcastTime(new Date("Sun, 28 Mar 2017 02:30:00 GMT"), Date.now())
-        .exec()
-        .then(announcements => {
-            res.send(announcements);
-            console.log(announcements);
-        })
-        .catch(err => {
-            console.error(err);
-        });
+    if (req.session.loggedIn && req.session.can_edit_announcement) {
+        Announcements.find()
+            .byBroadcastTime(new Date("Sun, 28 Mar 2017 02:30:00 GMT"), Date.now())
+            .exec()
+            .then(announcements => {
+                announcements.sort();
+                res.send(announcements);
+                console.log(announcements);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }
+    else {
+        Announcements.find()
+            .byBroadcastTime(new Date("Sun, 28 Mar 2017 02:30:00 GMT"), Date.now())
+            .byIsApproved()
+            .exec()
+            .then(announcements => {
+                res.send(announcements);
+                console.log(announcements);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }
 });
 
 module.exports = router;
