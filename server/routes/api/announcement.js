5297d104cb76191a071fadae4a8ea47a910ec60c
Andrew Boland <andrewbo@umich.edu>
2017-05-30 22:00:29 -0400

    Added beforeNow query

--- server/routes/api/announcement.js
+++ server/routes/api/announcement.js
@@ -8,7 +8,6 @@ function sortByDate(a,b) {
 router.get('/', function(req, res) {
     if (req.session.loggedIn && req.session.can_edit_announcement) {
         Announcements.find()
-            .byBroadcastTime(new Date("Sun, 28 Mar 2017 02:30:00 GMT"), Date.now())
             .exec()
             .then(announcements => {
                 announcements.sort(sortByDate);
@@ -22,6 +21,7 @@ router.get('/', function(req, res) {
     else {
         Announcements.find()
             .byIsPublic()
+            .beforeNow()
             .exec()
             .then(announcements => {
                 announcements.sort(sortByDate);
