942e6e2a682d7526691bd776ca2f182bbdc233ed
Connor Krupp <kruppcon@umich.edu>
2017-06-04 13:14:50 -0700

    Added homepage

---
+++ app/pages/home/section_components.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const SectionHeader = styled.h2`
+    fontSize: 40px;
+    color: darkorange;
+    fontWeight: 400;
+`;
+
+const SectionBody = styled.p`
+    color: gray;
+    fontSize: 20px;
+`;
+
+export { SectionHeader, SectionBody };
