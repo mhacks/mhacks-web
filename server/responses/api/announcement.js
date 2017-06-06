e0baa1578240fe10300d7950def357abfbd7a9a3
Andrew Boland <andrewbo@umich.edu>
2017-06-05 23:14:18 -0400

    Implemented put and patch

--- server/responses/api/announcement.js
+++ server/responses/api/announcement.js
@@ -2,7 +2,6 @@ module.exports = {
     PERMISSIONS_REQUIRED: 'You do not have the correct permissions.',
     PARAMS_NOT_FOUND: 'Unauthorized, some fields not set.',
     UNKNOWN_ERROR: 'There was an error with the request, please try again later',
-
     METHOD_NOT_ALLOWED: 'Method not allowed',
     SUCCESSFUL_AUTH: 'Successfully authenticated',
     INVALID_PASSWORD: 'Unauthorized, email and password combination is invalid.',
--- server/routes/api/announcement.js
+++ server/routes/api/announcement.js
@@ -12,30 +12,44 @@ router.get('/', function(req, res) {
             .exec()
             .then(announcements => {
                 announcements.sort(sortByDate);
-                res.send(announcements);
                 console.log(announcements);
+                res.send({
+                    status: true,
+                    announcements: announcements
+                });
             })
             .catch(err => {
                 console.error(err);
+                res.send({
+                    status: false,
+                    message: Responses.UNKNOWN_ERROR
+                });
             });
     }
     else {
         Announcement.find()
-            //.byIsPublic()
+            .byIsPublic()
             .exec()
             .then(announcements => {
                 announcements.sort(sortByDate);
-                res.send(announcements);
                 console.log(announcements);
+                res.send({
+                    status: true,
+                    announcements: announcements
+                });
             })
             .catch(err => {
                 console.error(err);
+                res.send({
+                    status: false,
+                    message: Responses.UNKNOWN_ERROR
+                });
             });
     }
 });
 
 router.post('/', function(req, res) {
-    if (true || req.session.loggedIn && req.session.can_edit_announcement) {
+    if (req.session.loggedIn && req.session.can_edit_announcement) {
         if (req.body.title && req.body.body && req.body.category) {
             Announcement.create({
                 title: req.body.title,
@@ -69,30 +83,31 @@ router.post('/', function(req, res) {
             message: Responses.PERMISSIONS_REQUIRED
         });
     }
-
 });
 
-router.patch('/', function(req, res) {
-    //update announcement conforming to PUT or PATCH semantics
-    if (true || req.session.loggedIn && req.session.can_edit_announcement) {
-        if (req.body.title) {
-            Announcement.update({
-                title: req.body.title
-            }, {
-                title: req.body.title,
-                body: req.body.body,
-                broadcastTime: req.body.broadcastTime,
-                category: req.body.category,
-                isApproved: req.body.isApproved,
-                isSent: req.body.isSent
-            }, function (err, raw) {
-                if (err) {
-                    console.log('wut', raw);
-                } else {
-                    res.send({
-                        status: true
-                    });
-                }
+router.put('/', function(req, res) {
+    if (req.session.loggedIn && req.session.can_edit_announcement) {
+        if (req.body.id) {
+            Announcement.findById(req.body.id)
+            .exec()
+            .then(announcement => {
+                announcement.title = req.body.title;
+                announcement.body = req.body.body;
+                announcement.broadcastTime = req.body.broadcastTime;
+                announcement.category = req.body.category;
+                announcement.isApproved = req.body.isApproved;
+                announcement.isSent = req.body.isSent;
+                announcement.save();
+                res.send({
+                    status: true
+                });
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(500).send({
+                    status: false,
+                    message: Responses.UNKNOWN_ERROR
+                });
             });
         } else {
             res.status(401).send({
@@ -100,12 +115,42 @@ router.patch('/', function(req, res) {
                 message: Responses.PARAMS_NOT_FOUND
             });
         }
+    } else {
+        res.status(401).send({
+            status: false,
+            message: Responses.PERMISSIONS_REQUIRED
+        });
     }
 });
 
 router.patch('/', function(req, res) {
-    //update announcement conforming to PUT or PATCH semantics
-    res.send('patch');
+    if (req.session.loggedIn && req.session.can_edit_announcement) {
+        if (req.body.id) {
+            Announcement.updateOne({ _id: req.body.id }, req.body, { runValidators: true })
+                .then(announcement => {
+                    res.send({
+                        status: true
+                    });
+                })
+                .catch(err => {
+                    console.error(err);
+                    res.status(500).send({
+                        status: false,
+                        message: Responses.UNKNOWN_ERROR
+                    });
+                });
+        } else {
+            res.status(401).send({
+                status: false,
+                message: Responses.PARAMS_NOT_FOUND
+            });
+        }
+    } else {
+        res.status(401).send({
+            status: false,
+            message: Responses.PERMISSIONS_REQUIRED
+        });
+    }
 });
 
 module.exports = router;
