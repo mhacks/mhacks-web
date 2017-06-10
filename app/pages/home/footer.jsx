7ec0901863a969b38e164c71c4650c39f6fd935b
Mariano Montori <mmontori@umich.edu>
2017-06-10 13:29:47 -0400

    simple footer changes (#149)
    * simple footer changes
    
    changed the footer to match the one on design. all I had to do was
    remove the header logo flexer, and add a flexer after the image buttons
    for fb, twitter, etc. Seems to work fine this way
    
    * copied footer over to home page
    
    * small size changes for footer
    
    -image smaller
    -text smaller
    
    * blackout footer back to original state
    
    -changed the footer for the blackout page back to before
    -nano footer text now matches gray color
    
    * Wrapper keeps footer within page
    
    -now footer stays within the page, doesn’t overflow out
    -no more vertical scrolling for entire page
    -css change to keep footer more consistent with mockup from design

--- app/pages/home/footer.jsx
+++ app/pages/home/footer.jsx
@@ -1,20 +1,115 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { Container } from '../../components';
+import { devices } from '../../styles';
+
+const InstagramImg = require('../../../static/icons/instagram.png');
+const FacebookImg = require('../../../static/icons/facebook.png');
+const TwitterImg = require('../../../static/icons/twitter.png');
+
+/* Footer Style */
 const FooterWrapper = styled.div`
     position: fixed;
     bottom: 0;
     left: 0;
     right: 0;
-
-    height: 90px;
-    backgroundColor: gray;
 `;
+
+const HomeFooter = styled.footer`
+    position: relative;
+    display: flex;
+    padding: 1rem;
+    alignContent: space-between;
+    alignItems: center;
+    flexWrap: wrap;
+`;
+
+const Text = styled.h2`
+    color: gray;
+    fontSize: 12px;
+    marginBottom: 0;
+    textAlign: left;
+    flex: 1;
+    textAlign: center;
+    minWidth: 100%;
+    order: 1;
+
+    ${devices.tablet`
+        textAlign: left;
+        textIndent: 50px;
+        minWidth: 0;
+        order: 0;
+    `}
+`;
+
+const Flexer = styled.div`
+    flex: 1;
+    position: relative;
+    display: flex;
+    justifyContent: center;
+    minWidth: 100%;
+
+    ${devices.tablet`
+        textAlign: left;
+        minWidth: 0;
+
+        ${props => (props.right ? 'justifyContent: flex-end;' : '')}
+    `}
+`;
+
+const Img100 = styled.img`
+    height: 100%;
+    margin: auto;
+`;
+
+const ImgButtonWrapper = styled.div`
+    height: 20px;
+    width: 20px;
+    margin: 5px;
+    textAlign: center;
+    display: block;
+`;
+
+const ImgButton = props => (
+    <ImgButtonWrapper>
+        <a href={props.href}>
+            <Img100 {...props} />
+        </a>
+    </ImgButtonWrapper>
+);
 
 class Footer extends React.Component {
     render() {
         return (
-            <FooterWrapper />
+            <Container>
+                <FooterWrapper>
+                    <HomeFooter>
+                        <Text>© MHacks 2017</Text>
+                        <Flexer>
+                            <ImgButton
+                                src={FacebookImg}
+                                alt="Facebook"
+                                align="middle"
+                                href="https://www.facebook.com/MHacksHackathon"
+                            />
+                            <ImgButton
+                                src={InstagramImg}
+                                alt="Instagram"
+                                align="middle"
+                                href="https://www.instagram.com/mhacks_"
+                            />
+                            <ImgButton
+                                src={TwitterImg}
+                                alt="Twitter"
+                                align="middle"
+                                href="https://twitter.com/mhacks"
+                            />
+                        </Flexer>
+                        <Flexer/>
+                    </HomeFooter>
+                </FooterWrapper>
+            </Container>
         );
     }
 }
