56084ea74a6aab304c2ffe0d4b460a90228943ee
Antonio Mika <antoniomika@gmail.com>
2017-06-11 16:34:39 -0400

    Fixed endpoint urls

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -151,8 +151,7 @@ router.get('/verify/:token', function(req, res) {
                     });
             }
         })
-        .catch(() => {
-        });
+        .catch(() => {});
     res.redirect('/profile');
 });
 
