fa5dbc43b5234d6b36aad64ae9bd4ccc34caafa2
Andrew <andrewbo@umich.edu>
2017-06-11 12:52:12 -0400

    Fixed content (#172)

--- app/pages/home/index.jsx
+++ app/pages/home/index.jsx
@@ -96,7 +96,7 @@ class HomePage extends React.Component {
                     <Content>
                         <div id="about">
                             <SectionHeader>About MHacks</SectionHeader>
-                            <SectionBody>MHacks is a 36-hour hackathon run by University of Michigan students. At MHacks, we want to help you turn your ideas into reality. You're welcome to come with or without a team. We'll provide you with all the resources and mentors you need to help you work on something cool or learn new skills. You'll have the freedom to create a product, learn new techniques for your future work, or just have fun working on a project with friends. Moreover, we will offer you a chance to network with other creators and professionals. For newcomers and veterans alike, MHacks is a weekend experience you won’t want to miss.</SectionBody>
+                            <SectionBody>MHacks is a group of University of Michigan students who want to help you turn your ideas into reality. We're best known for the 36-hour hackathons we run twice a year. You're welcome to come with or without a team. We'll provide you with all the resources and mentors you need to help you work on something cool or learn new skills. You'll have the freedom to create a product, learn new techniques for your future work, or just have fun working on a project with friends. Moreover, we will offer you a chance to network with other creators and professionals. For newcomers and veterans alike, MHacks events are experiences you won’t want to miss.</SectionBody>
                         </div>
 
                         <div id="what-is-mhacks-nano">
