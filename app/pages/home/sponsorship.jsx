a4e464c25c40c694968275216f354f855a0093f9
Mariano <mmontori@umich.edu>
2017-06-11 20:33:38 -0400

    updated sponsorship text

--- app/pages/home/sponsorship.jsx
+++ app/pages/home/sponsorship.jsx
@@ -7,10 +7,12 @@ class Sponsorship extends React.Component {
 			<div>
 				<SectionHeader>Sponsorship</SectionHeader>
 				<SectionBody> MHacks is the gateway between you and the top tech talent in the world. Whether you come to network, promote brand awareness, or get feedback on a product or API, we’ll provide the tools you need to make the most out of your experience. </SectionBody>
-				<SectionBody> Interested in sponsoring? Want more information? Shoot us an email at kevin@mhacks.org </SectionBody>
+				<SectionBody> Interested in sponsoring? Want more information? Our next major event will be in the Fall and we'd love to hear from you! Shoot us an email at kevin@mhacks.org. </SectionBody>
 			</div>
 		);
 	}
 }
 
 export default Sponsorship;
+
+
