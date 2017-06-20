d7eb62d6aa39611658e3bce077b6a94073a55dc4
Robert Allen <robby.allen24@gmail.com>
2017-06-19 21:55:14 -0400

    token connected

--- app/pages/live/chat/index.jsx
+++ app/pages/live/chat/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-export default class Chat extends React.Component {
+class Chat extends React.Component {
     render(){
+        console.log(this.props.token)
         return (
             <div>chat here</div>
         );
     }
 }
+
+function mapStateToProps(state){
+    return {
+        token: state.userState.data.token
+    }
+}
+
+export default connect(mapStateToProps)(Chat);
