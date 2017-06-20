d4e651a93e095516d5d818b50970dd5fa0dc1e4a
Antonio Mika <antoniomika@gmail.com>
2017-06-19 22:34:49 -0400

    Added messages stuff

--- app/pages/live/chat/index.jsx
+++ app/pages/live/chat/index.jsx
@@ -1,9 +1,63 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import io from 'socket.io-client';
 
 class Chat extends React.Component {
+    componentDidMount() {
+        this.socket = io.connect(window.location.origin, {
+            reconnection: false
+        });
+
+        let component = this;
+
+        this.socket.on('authenticate', function (data) {
+            console.log('Authenticate', data);
+            if (!data) {
+                component.authenticate(this.props.token);
+            } else {
+                component.retrieveProfile();
+            }
+        });
+
+        this.socket.on('status', function (data) {
+            console.log('Status', data);
+        });
+
+        this.socket.on('chat', function (data) {
+            console.log('Chat', data);
+        });
+
+        this.socket.on('channels', function (data) {
+            console.log('Channels', data);
+        });
+    }
+
+    sendMessage(message, channel) {
+        this.socket.emit('chat', {
+            message,
+            channel
+        });
+    }
+
+    retrieveProfile() {
+        this.socket.emit('profile');
+        this.socket.on('profile', function (data) {
+            console.log('Profile', data);
+            this.profileData = data;
+        });
+    }
+
+    authenticate(token) {
+        this.socket.emit('authenticate', {
+            token
+        });
+    }
+
+    componentWillUnmount() {
+        this.socket.disconnect();
+    }
+
     render(){
-        console.log(this.props.token)
         return (
             <div>chat here</div>
         );
