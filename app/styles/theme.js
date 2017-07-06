041c3fd6e1f88a85e3f2ac86d48cfbb58456ded0
Connor Krupp <kruppcon@umich.edu>
2017-07-05 17:38:39 -0700

    Fix prettier

--- app/styles/theme.js
+++ app/styles/theme.js
@@ -8,7 +8,8 @@ export default {
     success: '#2ecc71',
     teal: '#1CA3A3',
     darkPink: '#CA2171',
-    backgroundGradient: '-webkit-linear-gradient(45deg, rgba(240,47,23,1) 0%, rgba(246,41,12,1) 0%, rgba(241,111,92,1) 0%, rgba(253,177,154,1) 0%, rgba(197,99,250,1) 100%)',
+    backgroundGradient:
+        '-webkit-linear-gradient(45deg, rgba(240,47,23,1) 0%, rgba(246,41,12,1) 0%, rgba(241,111,92,1) 0%, rgba(253,177,154,1) 0%, rgba(197,99,250,1) 100%)',
 
     // Font sizes
     headerFontSize: '50px',
