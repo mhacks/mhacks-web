ce562d111838a103c1b2b23b6a671b86a68cc4ad
Andrew Boland <andrewbo@umich.edu>
2017-07-05 22:55:21 -0400

    Added styled content

--- app/pages/home/sponsorship.jsx
+++ app/pages/home/sponsorship.jsx
@@ -24,6 +24,27 @@ const FlexBox = styled.div`
     justifyContent: center;
 `;
 
+const Quote = styled.div`
+    margin: 20px auto;
+    display: ;
+    justifyContent: center;
+`;
+
+const QuoteText = styled.div`
+    color: ${theme.highlight};
+    fontSize: 20px;
+`;
+
+const CompanyName = styled.div`
+    color: ${theme.highlight};
+    fontWeight: bold;
+    fontSize: 25px;
+`;
+
+const Link = styled.a`
+    color: white;
+`;
+
 class Sponsorship extends React.Component {
 	render() {
 		return (
@@ -31,13 +52,25 @@ class Sponsorship extends React.Component {
                 <Container>
                     <SectionHeader>Sponsorship</SectionHeader>
                     <SectionBody> MHacks is the gateway between you and the top tech talent in the world. Whether you come to network, promote brand awareness, or get feedback on a product or API, we’ll provide the tools you need to make the most out of your experience. </SectionBody>
-                    <SectionBody> Interested in sponsoring? Want more information? Our next major event will be in the Fall and we'd love to hear from you! Shoot us an email at kevin@mhacks.org. </SectionBody>
-                    <SectionBody> We have an awesome lineup of sponsors for you! Check back soon for logos. </SectionBody>
+                    <SectionBody> Interested in sponsoring? Want more information? Our next major event will be in the Fall and we'd love to hear from you! Shoot us an email at <Link href="mailto:kevin@mhacks.org">kevin@mhacks.org</Link> </SectionBody>
+
+                    <SectionBody> We have an awesome lineup of sponsors for you. Check back soon for company logos. </SectionBody>
+
                     <FlexBox>
                         <PlaceHolderImage />
                         <PlaceHolderImage />
                         <PlaceHolderImage />
                     </FlexBox>
+
+                    <Quote>
+                        <QuoteText> “This was one of the most well organized student-run hackathons that we’ve ever sponsored.” </QuoteText>
+                        <CompanyName> -Google</CompanyName>
+                    </Quote>
+
+                    <Quote>
+                        <QuoteText> “The caliber and number of students that MHacks attracts is like no other hacking event.” </QuoteText>
+                        <CompanyName> -Walmart</CompanyName>
+                    </Quote>
                 </Container>
 
 			</Wrapper>
