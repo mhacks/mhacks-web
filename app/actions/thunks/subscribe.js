3dbf7a208b0b7ac109557dc8ba5cd2b03b2ad7d9
Robert Allen <robby.allen24@gmail.com>
2017-08-01 22:11:10 -0400

    Redux cleanup (#294)
    * redux-actions cleanup
    
    * prettier
    
    * prettier 2

--- app/actions/thunks/subscribe.js
+++ app/actions/thunks/subscribe.js
@@ -1,30 +1,31 @@
-import { SubscribePureActions } from '../pure';
+import { actions } from '../../actions';
 import { SubscribeRequests } from '../requests';
 
 export default class SubscribeThunks {
     static subscribe(email) {
         return dispatch => {
-            dispatch(SubscribePureActions.subscribeRequest(email));
+            dispatch({
+                type: actions.SUBSCRIBE_REQUEST,
+                data: email
+            });
 
             return SubscribeRequests.subscribe({ email }).then(response => {
                 if (response.status == 200) {
                     response.json().then(json => {
-                        dispatch(
-                            SubscribePureActions.subscribeSuccess(
-                                email,
-                                json.message
-                            )
-                        );
+                        dispatch({
+                            type: actions.SUBSCRIBE_SUCCESS,
+                            data: email,
+                            message: json.message
+                        });
                     });
                 } else {
                     response.json().then(json => {
-                        dispatch(
-                            SubscribePureActions.subscribeError(
-                                email,
-                                response.status,
-                                json.message
-                            )
-                        );
+                        dispatch({
+                            type: actions.SUBSCRIBE_ERROR,
+                            data: email,
+                            error: response.status,
+                            message: json.message
+                        });
                     });
                 }
             });
--- app/constants/index.js
+++ app/constants/index.js
@@ -1,6 +1,5 @@
-import reduxActions from './redux_actions';
 import routes from './routes';
 import endpoints from './endpoints';
 import forms from './forms';
 
-export { reduxActions, routes, endpoints, forms };
+export { routes, endpoints, forms };
