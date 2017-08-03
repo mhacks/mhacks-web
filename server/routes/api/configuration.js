91f639095fdbbc4a33ae37b8a511034e7febd96b
Andrew Boland <andrewbo@umich.edu>
2017-08-02 20:35:31 -0400

    Simplified to just get and post and fixed requests

--- server/routes/api/configuration.js
+++ server/routes/api/configuration.js
@@ -22,7 +22,7 @@ router.get('/', function(req, res) {
         })
         .catch(err => {
             console.error(err);
-            res.send({
+            res.status(500).send({
                 status: false,
                 message: Responses.UNKNOWN_ERROR
             });
@@ -32,56 +32,59 @@ router.get('/', function(req, res) {
 // Handles post requests for /v1/configuration/control
 //This is on route /configuration/control so it is not easily accessible (it should be limited to one configuration at a time)
 router.post('/control', authMiddleware('admin', 'api'), function(req, res) {
-    if (req.body.app_name && req.body.start_date && req.body.end_date) {
-        ConfigurationSchema.create({
-            app_name: req.body.app_name,
-            start_date: req.body.start_date,
-            end_date: req.body.end_date,
-            is_live_page_enabled: req.body.is_live_page_enabled,
-            is_application_open: req.body.is_application_open
-        })
-            .then(() => {
-                res.send({
-                    status: true
-                });
-            })
-            .catch(err => {
-                console.error(err);
-                res.status(500).send({
-                    status: false,
-                    message: Responses.UNKNOWN_ERROR
-                });
-            });
-    } else {
-        res.status(401).send({
-            status: false,
-            message: Responses.PARAMS_NOT_FOUND
-        });
-    }
-});
-
-// Handles put requests for /v1/configuration/control
-router.put('/control', authMiddleware('admin', 'api'), function(req, res) {
     ConfigurationSchema.findOne()
         .exec()
         .then(configuration => {
-            configuration.app_name =
-                req.body.app_name || configuration.app_name;
-            configuration.start_date =
-                req.body.start_date || configuration.start_date;
-            configuration.end_date =
-                req.body.end_date || configuration.end_date;
-            configuration.is_live_page_enabled =
-                req.body.is_live_page_enabled ||
-                configuration.is_live_page_enabled;
-            configuration.is_application_open =
-                req.body.is_application_open ||
-                configuration.is_application_open;
+            if (configuration) {
+                configuration.app_name =
+                    req.body.app_name || configuration.app_name;
+                configuration.start_date =
+                    req.body.start_date || configuration.start_date;
+                configuration.end_date =
+                    req.body.end_date || configuration.end_date;
+                configuration.is_live_page_enabled =
+                    req.body.is_live_page_enabled ||
+                    configuration.is_live_page_enabled;
+                configuration.is_application_open =
+                    req.body.is_application_open ||
+                    configuration.is_application_open;
 
-            configuration.save();
-            res.send({
-                status: true
-            });
+                configuration.save();
+                res.send({
+                    status: true
+                });
+            } else {
+                if (
+                    req.body.app_name &&
+                    req.body.start_date &&
+                    req.body.end_date
+                ) {
+                    ConfigurationSchema.create({
+                        app_name: req.body.app_name,
+                        start_date: req.body.start_date,
+                        end_date: req.body.end_date,
+                        is_live_page_enabled: req.body.is_live_page_enabled,
+                        is_application_open: req.body.is_application_open
+                    })
+                        .then(() => {
+                            res.send({
+                                status: true
+                            });
+                        })
+                        .catch(err => {
+                            console.error(err);
+                            res.status(500).send({
+                                status: false,
+                                message: Responses.UNKNOWN_ERROR
+                            });
+                        });
+                } else {
+                    res.status(401).send({
+                        status: false,
+                        message: Responses.PARAMS_NOT_FOUND
+                    });
+                }
+            }
         })
         .catch(err => {
             console.error(err);
@@ -90,33 +93,6 @@ router.put('/control', authMiddleware('admin', 'api'), function(req, res) {
                 message: Responses.UNKNOWN_ERROR
             });
         });
-});
-
-// Handles delete requests for /v1/configuration/control
-router.delete('/control', authMiddleware('admin', 'api'), function(req, res) {
-    if (req.body.app_name) {
-        ConfigurationSchema.find()
-            .byAppName(req.body.app_name)
-            .exec()
-            .then(configuration => {
-                configuration.remove();
-                res.send({
-                    status: true
-                });
-            })
-            .catch(err => {
-                console.error(err);
-                res.status(500).send({
-                    status: false,
-                    message: Responses.UNKNOWN_ERROR
-                });
-            });
-    } else {
-        res.status(401).send({
-            status: false,
-            message: Responses.PARAMS_NOT_FOUND
-        });
-    }
 });
 
 module.exports = router;
