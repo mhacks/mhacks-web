2c4c223ffb8a5341beb079e49da4206e3711b45d
Andrew <andrewbo@umich.edu>
2017-08-03 13:16:08 -0400

    Configuration model (#290)
    * Created config schema
    
    * Added post and get requests
    
    * Added put and delete routes
    
    * Fixed requested changes
    
    * Fixed prettier for profile I guess
    
    * Enabled ESLint
    
    * Simplified to just get and post and fixed requests

--- server/routes/api/configuration.js
+++ server/routes/api/configuration.js
@@ -1,43 +1,98 @@
 var router = require('express').Router(),
-    User = require('../../db/model/User.js');
+    ConfigurationSchema = require('../../db/model/Configuration.js'),
+    authMiddleware = require('../../middleware/auth.js'),
+    Responses = require('../../responses/api/announcement.js');
 
 // Handles get requests for /v1/configuration
 router.get('/', function(req, res) {
-    const configuration = {
-        is_livepage_enabled: false,
-        is_applications_open: true
-    };
-
-    if (req.get('Authorization')) {
-        var authorization = req.get('Authorization');
-        var token = authorization.replace(/Bearer /gi, '');
-
-        User.find()
-            .byToken(token)
-            .exec()
-            .then(user => {
-                user.getProfile().then(profile => {
-                    res.send({
-                        status: true,
-                        user: profile,
-                        configuration
-                    });
+    ConfigurationSchema.findOne({}, '-_id -__v')
+        .exec()
+        .then(configuration => {
+            if (configuration) {
+                res.send({
+                    status: true,
+                    configuration: configuration
                 });
-            })
-            .catch(() => {
-                configuration['should_logout'] = true;
-
+            } else {
                 res.send({
                     status: false,
-                    configuration
+                    configuration: {}
                 });
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).send({
+                status: false,
+                message: Responses.UNKNOWN_ERROR
             });
-    } else {
-        res.send({
-            status: false,
-            configuration
         });
-    }
+});
+
+// Handles post requests for /v1/configuration/control
+//This is on route /configuration/control so it is not easily accessible (it should be limited to one configuration at a time)
+router.post('/control', authMiddleware('admin', 'api'), function(req, res) {
+    ConfigurationSchema.findOne()
+        .exec()
+        .then(configuration => {
+            if (configuration) {
+                configuration.app_name =
+                    req.body.app_name || configuration.app_name;
+                configuration.start_date =
+                    req.body.start_date || configuration.start_date;
+                configuration.end_date =
+                    req.body.end_date || configuration.end_date;
+                configuration.is_live_page_enabled =
+                    req.body.is_live_page_enabled ||
+                    configuration.is_live_page_enabled;
+                configuration.is_application_open =
+                    req.body.is_application_open ||
+                    configuration.is_application_open;
+
+                configuration.save();
+                res.send({
+                    status: true
+                });
+            } else {
+                if (
+                    req.body.app_name &&
+                    req.body.start_date &&
+                    req.body.end_date
+                ) {
+                    ConfigurationSchema.create({
+                        app_name: req.body.app_name,
+                        start_date: req.body.start_date,
+                        end_date: req.body.end_date,
+                        is_live_page_enabled: req.body.is_live_page_enabled,
+                        is_application_open: req.body.is_application_open
+                    })
+                        .then(() => {
+                            res.send({
+                                status: true
+                            });
+                        })
+                        .catch(err => {
+                            console.error(err);
+                            res.status(500).send({
+                                status: false,
+                                message: Responses.UNKNOWN_ERROR
+                            });
+                        });
+                } else {
+                    res.status(401).send({
+                        status: false,
+                        message: Responses.PARAMS_NOT_FOUND
+                    });
+                }
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).send({
+                status: false,
+                message: Responses.UNKNOWN_ERROR
+            });
+        });
 });
 
 module.exports = router;
