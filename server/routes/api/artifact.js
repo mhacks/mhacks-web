e0899272a26338d64c801bc127db14183e626a60
Antonio Mika <antoniomika@gmail.com>
2017-08-08 18:45:55 -0400

    Fixed tests

--- server/routes/api/artifact.js
+++ server/routes/api/artifact.js
@@ -5,29 +5,30 @@ var router = require('express').Router(),
     authMiddleware = require('../../middleware/auth.js'),
     mime = require('mime');
 
-router.get('/resume/:email', authMiddleware('reader sponsor admin', 'api'), function(
-    req,
-    res
-) {
-    if (req.params.email && validator.isEmail(req.params.email)) {
-        artifact(req.params.email, 'resume', req.query.application)
-            .then(stream => {
-                res.attachment(stream[0]);
-                res.send(stream[1].data.Body);
-            })
-            .catch(err => {
-                res.status(500).send({
-                    status: false,
-                    message: err.message
+router.get(
+    '/resume/:email',
+    authMiddleware('reader sponsor admin', 'api'),
+    function(req, res) {
+        if (req.params.email && validator.isEmail(req.params.email)) {
+            artifact(req.params.email, 'resume', req.query.application)
+                .then(stream => {
+                    res.attachment(stream[0]);
+                    res.send(stream[1].data.Body);
+                })
+                .catch(err => {
+                    res.status(500).send({
+                        status: false,
+                        message: err.message
+                    });
                 });
+        } else {
+            res.status(404).send({
+                status: false,
+                message: Responses.NOT_FOUND
             });
-    } else {
-        res.status(404).send({
-            status: false,
-            message: Responses.NOT_FOUND
-        });
+        }
     }
-});
+);
 
 router.get('/avatar/:email', function(req, res) {
     if (req.params.email && validator.isEmail(req.params.email)) {
