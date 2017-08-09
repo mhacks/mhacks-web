aeea20b328565c1fa05fd3112f649a977c6f5f40
Antonio Mika <antoniomika@gmail.com>
2017-08-08 23:54:59 -0400

    Fixed artifacts to upload and store locally without aws

--- server/interactors/artifact.js
+++ server/interactors/artifact.js
@@ -2,92 +2,168 @@ var AWS = require('aws-sdk'),
     config = require('../../config/default.js'),
     User = require('../db/model/User.js'),
     Responses = require('../responses/api/index.js'),
-    Application = require('../db/model/Application.js');
+    Application = require('../db/model/Application.js'),
+    fs = require('fs');
 
 module.exports = function(email, type, application) {
-    var s3 = new AWS.S3({
-        accessKeyId: config.AWS_ACCESS_KEY_ID,
-        secretAccessKey: config.AWS_SECRET_ACCESS_KEY
-    });
-
     return new Promise((resolve, reject) => {
-        if (application) {
-            Application.findOne({ user: email })
-                .exec()
-                .then(application => {
-                    if (type in application) {
-                        var directory = '';
-                        var url = application[type];
-                        var fileName = url.split('/').pop();
+        var s3 = false;
 
-                        if (type === 'resume') {
-                            directory = 'resumes';
-                        } else {
-                            directory = 'avatars';
-                        }
-
-                        var params = {
-                            Bucket: config.AWS_BUCKET_NAME,
-                            Key: directory + '/' + fileName
-                        };
-
-                        var obj = s3.getObject(params);
-
-                        obj
-                            .on('error', function(error) {
-                                reject(error);
-                            })
-                            .on('success', function(response) {
-                                resolve([fileName, response]);
-                            });
-
-                        obj.send();
-                    } else {
-                        reject(new Error(Responses.INVALID_TYPE));
-                    }
-                })
-                .catch(() => {
-                    reject(new Error(Responses.INVALID_TYPE));
-                });
-        } else {
-            User.find()
-                .byEmail(email)
-                .exec()
-                .then(user => {
-                    if (type in user) {
-                        var directory = '';
-                        var url = user[type];
-                        var fileName = url.split('/').pop();
-
-                        if (type === 'resume') {
-                            directory = 'resumes';
-                        } else {
-                            directory = 'avatars';
-                        }
-
-                        var params = {
-                            Bucket: config.AWS_BUCKET_NAME,
-                            Key: directory + '/' + fileName
-                        };
-
-                        var obj = s3.getObject(params);
-
-                        obj
-                            .on('error', function(error) {
-                                reject(error);
-                            })
-                            .on('success', function(response) {
-                                resolve([fileName, response]);
-                            });
-
-                        obj.send();
-                    } else {
-                        reject(new Error(Responses.INVALID_TYPE));
-                    }
-                })
-                .catch(() => {
-                    reject(new Error(Responses.INVALID_TYPE));
-                });
+        if (config.AWS_ACCESS_KEY_ID && config.AWS_SECRET_ACCESS_KEY) {
+            s3 = new AWS.S3({
+                accessKeyId: config.AWS_ACCESS_KEY_ID,
+                secretAccessKey: config.AWS_SECRET_ACCESS_KEY
+            });
         }
+
+        User.find()
+            .byEmail(email)
+            .exec()
+            .then(user => {
+                if (user) {
+                    if (application) {
+                        Application.findOne({ user: email })
+                            .exec()
+                            .then(application => {
+                                if (type in application) {
+                                    var directory = '';
+                                    var url = application[type] || '';
+                                    var fileName = url.split('/').pop();
+                                    var extension = fileName.split('.').pop();
+
+                                    if (type === 'resume') {
+                                        directory = 'resumes';
+                                    } else {
+                                        directory = 'avatars';
+                                    }
+
+                                    if (s3) {
+                                        var params = {
+                                            Bucket: config.AWS_BUCKET_NAME,
+                                            Key: directory + '/' + fileName
+                                        };
+
+                                        var obj = s3.getObject(params);
+
+                                        obj
+                                            .on('error', function(error) {
+                                                reject(error);
+                                            })
+                                            .on('success', function(response) {
+                                                resolve([
+                                                    user.full_name +
+                                                        '.' +
+                                                        extension,
+                                                    response,
+                                                    fileName
+                                                ]);
+                                            });
+                                        obj.send();
+                                    } else {
+                                        if (
+                                            fs.existsSync(
+                                                'build/uploads/' +
+                                                    directory +
+                                                    '/' +
+                                                    fileName
+                                            )
+                                        ) {
+                                            resolve([
+                                                user.full_name +
+                                                    '.' +
+                                                    extension,
+                                                {
+                                                    data: {
+                                                        Body: fs.readFileSync(
+                                                            'build/uploads/' +
+                                                                directory +
+                                                                '/' +
+                                                                fileName
+                                                        )
+                                                    }
+                                                },
+                                                fileName
+                                            ]);
+                                        } else {
+                                            reject(
+                                                new Error(Responses.NOT_FOUND)
+                                            );
+                                        }
+                                    }
+                                } else {
+                                    reject(new Error(Responses.INVALID_TYPE));
+                                }
+                            })
+                            .catch(err => {
+                                reject(err);
+                            });
+                    } else {
+                        if (type in user) {
+                            var directory = '';
+                            var url = user[type] || '';
+                            var fileName = url.split('/').pop();
+                            var extension = fileName.split('.').pop();
+
+                            if (type === 'resume') {
+                                directory = 'resumes';
+                            } else {
+                                directory = 'avatars';
+                            }
+
+                            var params = {
+                                Bucket: config.AWS_BUCKET_NAME,
+                                Key: directory + '/' + fileName
+                            };
+
+                            if (s3) {
+                                var obj = s3.getObject(params);
+
+                                obj
+                                    .on('error', function(error) {
+                                        reject(error);
+                                    })
+                                    .on('success', function(response) {
+                                        resolve([fileName, response]);
+                                    });
+
+                                obj.send();
+                            } else {
+                                if (
+                                    fs.existsSync(
+                                        'build/uploads/' +
+                                            directory +
+                                            '/' +
+                                            fileName
+                                    )
+                                ) {
+                                    resolve([
+                                        user.full_name + '.' + extension,
+                                        {
+                                            data: {
+                                                Body: fs.readFileSync(
+                                                    'build/uploads/' +
+                                                        directory +
+                                                        '/' +
+                                                        fileName
+                                                )
+                                            }
+                                        },
+                                        fileName
+                                    ]);
+                                } else {
+                                    reject(new Error(Responses.NOT_FOUND));
+                                }
+                            }
+                        } else {
+                            reject(new Error(Responses.INVALID_TYPE));
+                        }
+                    }
+                } else {
+                    reject(new Error(Responses.INVALID_TYPE));
+                }
+            })
+            .catch(err => {
+                reject(err);
+            });
     });
 };
