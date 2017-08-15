f702187f73ccab6f416a8832deda447d4c6d01d5
Antonio Mika <antoniomika@gmail.com>
2017-08-14 21:50:51 -0400

    Prettier fixes

--- server/routes/api/mentor.js
+++ server/routes/api/mentor.js
@@ -6,7 +6,10 @@ var router = require('express').Router(),
         config.AWS_BUCKET_NAME
     );
 
-router.post('/application', uploadHelper.fields([{ name: 'resume' }]), function(req, res) {
+router.post('/application', uploadHelper.fields([{ name: 'resume' }]), function(
+    req,
+    res
+) {
     var updateable_fields = MentorApplication.getUpdateableFields(req.groups);
     var fields = {};
 
@@ -28,38 +31,41 @@ router.post('/application', uploadHelper.fields([{ name: 'resume' }]), function(
         }
     }
 
-    MentorApplication.find().byUser(req.user).then(application => {
-        if (application) {
-            application.updateFields(fields);
+    MentorApplication.find()
+        .byUser(req.user)
+        .then(application => {
+            if (application) {
+                application.updateFields(fields);
 
-            res.send({
-                status: true,
-                mentor_application: application
-            });
-        } else {
-            fields.user = req.user;
-            MentorApplication.create(fields)
-                .then(application => {
-                    res.send({
-                        status: true,
-                        mentor_application: application
-                    });
-                })
-                .catch(err => {
-                    console.error(err);
-                    res.status(500).send({
-                        status: false,
-                        message: Responses.UNKNOWN_ERROR
-                    });
+                res.send({
+                    status: true,
+                    mentor_application: application
                 });
-        }
-    }).catch(err => {
-        console.error(err);
-        res.status(500).send({
-            status: false,
-            message: Responses.UNKNOWN_ERROR
+            } else {
+                fields.user = req.user;
+                MentorApplication.create(fields)
+                    .then(application => {
+                        res.send({
+                            status: true,
+                            mentor_application: application
+                        });
+                    })
+                    .catch(err => {
+                        console.error(err);
+                        res.status(500).send({
+                            status: false,
+                            message: Responses.UNKNOWN_ERROR
+                        });
+                    });
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).send({
+                status: false,
+                message: Responses.UNKNOWN_ERROR
+            });
         });
-    });
 });
 
 // Returns mentor application for the current user
