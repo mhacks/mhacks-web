614a6fb699a32539c8845b250a0aa23a7fb67fd2
Antonio Mika <antoniomika@gmail.com>
2017-08-29 13:11:27 -0400

    suh duh

--- app/components/ProfilePicture.jsx
+++ app/components/ProfilePicture.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Avatar = styled.img`
-    width: inherit; 
-    minWidth: 100px;   
+    width: inherit;
+    minWidth: 100px;
     maxWidth: 200px;
 `;
 
