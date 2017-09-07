5038fdbb7759de4f85586efee19bf87916dad15b
Connor Krupp <kruppcon@umich.edu>
2017-09-07 00:06:00 -0400

    Add dashboard link to MHacks facebook event

--- app/pages/dashboard/application_section.jsx
+++ app/pages/dashboard/application_section.jsx
@@ -70,17 +70,24 @@ class ApplicationSection extends React.Component {
                 We’re excited to see you at MHacks X! We’ve got some great
                 things in store :) In the meantime, we encourage you to connect
                 with other hackers in the HH MHacks Facebook Group or{' '}
-                <Link href="https://www.facebook.com/events/363750477376350/">
+                <Link
+                    target="_blank"
+                    href="https://www.facebook.com/events/363750477376350/"
+                >
                     MHacks X Facebook Event
                 </Link>
                 . As always,stay tuned to our{' '}
-                <Link href="http://facebook.com/MHacksHackathon">
+                <Link
+                    target="_blank"
+                    href="http://facebook.com/MHacksHackathon"
+                >
                     Facebook
-                </Link>, <Link href="http://twitter.com/mhacks">Twitter</Link>,
-                and <Link href="http://instagram.com/mhacks_">
-                    Instagram
-                </Link>{' '}
-                for updates on all things MHacks.
+                </Link>,{' '}
+                <Link target="_blank" href="http://twitter.com/mhacks">
+                    Twitter
+                </Link>, and{' '}
+                <Link href="http://instagram.com/mhacks_">Instagram</Link> for
+                updates on all things MHacks.
             </SectionBody>
         );
     }
@@ -91,12 +98,19 @@ class ApplicationSection extends React.Component {
                 Unfortunately, we are unable to extend an invitation for MHacks
                 X to you at this time. We hope you will still be a part of our
                 community via{' '}
-                <Link href="http://facebook.com/MHacksHackathon">
+                <Link
+                    target="_blank"
+                    href="http://facebook.com/MHacksHackathon"
+                >
                     Facebook
-                </Link>, <Link href="http://twitter.com/mhacks">Twitter</Link>,{' '}
-                <Link href="http://instagram.com/mhacks_">Instagram</Link>, and
-                and encourage you to apply for the next MHacks event in the
-                winter.
+                </Link>,{' '}
+                <Link target="_blank" href="http://twitter.com/mhacks">
+                    Twitter
+                </Link>,{' '}
+                <Link target="_blank" href="http://instagram.com/mhacks_">
+                    Instagram
+                </Link>, and and encourage you to apply for the next MHacks
+                event in the winter.
             </SectionBody>
         );
     }
