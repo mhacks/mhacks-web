15eca041d391aacd99cef19a494f7cb100e7e345
Antonio Mika <antoniomika@gmail.com>
2017-09-14 23:57:17 -0400

    Final fixes

--- server/routes/api/device.js
+++ server/routes/api/device.js
@@ -3,101 +3,71 @@ var router = require('express').Router(),
     Responses = require('../../responses/api/index.js'),
     Device = require('../../db/model/Device.js');
 
-router.post(
-    '/',
-    authMiddleware(
-        'any',
-        'api',
-        false,
-        function(req, res) {
-            if (req.body.push_id) {
-                if (
-                    req.body.push_categories &&
-                    !Array.isArray(req.body.push_categories)
-                ) {
-                    req.body.push_categories = req.body.push_categories
-                        .split(',')
-                        .map(data => {
-                            return data.replace(/\s+/g, '');
-                        });
-                }
-
-                Device.findOne({
-                    push_id: req.body.push_id
-                })
-                    .exec()
-                    .then(device => {
-                        if (device) {
-                            var updateable_fields = Device.getUpdateableFields();
-                            var fields = {};
-
-                            for (var i in req.body) {
-                                if (updateable_fields.indexOf(i) !== -1) {
-                                    fields[i] = req.body[i];
-                                }
-                            }
-
-                            device.updateFields(fields);
-
-                            res.send({
-                                status: true,
-                                device: Object.assign({}, device.toJSON(), {
-                                    user: undefined
-                                })
-                            });
-                        } else {
-                            Device.create({
-                                push_id: req.body.push_id,
-                                push_categories: req.body.push_categories || []
-                            })
-                                .then(device => {
-                                    res.send({
-                                        status: true,
-                                        device: Object.assign(
-                                            {},
-                                            device.toJSON(),
-                                            {
-                                                user: undefined
-                                            }
-                                        )
-                                    });
-                                })
-                                .catch(err => {
-                                    res.send({
-                                        status: false,
-                                        message: err
-                                    });
-                                });
-                        }
-                    })
-                    .catch(err => {
-                        res.send({
-                            status: false,
-                            message: err
-                        });
-                    });
-            } else {
-                res.status(400).send({
-                    status: false,
-                    message: Responses.MISSING_PARAMETERS
+router.post('/', function(req, res) {
+    if (req.body.push_id) {
+        if (
+            req.body.push_categories &&
+            !Array.isArray(req.body.push_categories)
+        ) {
+            req.body.push_categories = req.body.push_categories
+                .split(',')
+                .map(data => {
+                    return data.replace(/\s+/g, '');
                 });
-            }
-        },
-        false
-    ),
-    function(req, res) {
-        if (req.body.push_id) {
-            if (
-                req.body.push_categories &&
-                !Array.isArray(req.body.push_categories)
-            ) {
-                req.body.push_categories = req.body.push_categories
-                    .split(',')
-                    .map(data => {
-                        return data.replace(/\s+/g, '');
-                    });
-            }
+        }
 
+        authMiddleware('any', 'api', false, function() {
+            Device.findOne({
+                push_id: req.body.push_id
+            })
+                .exec()
+                .then(device => {
+                    if (device) {
+                        var updateable_fields = Device.getUpdateableFields();
+                        var fields = {};
+
+                        for (var i in req.body) {
+                            if (updateable_fields.indexOf(i) !== -1) {
+                                fields[i] = req.body[i];
+                            }
+                        }
+
+                        device.updateFields(fields);
+
+                        res.send({
+                            status: true,
+                            device: Object.assign({}, device.toJSON(), {
+                                user: undefined
+                            })
+                        });
+                    } else {
+                        Device.create({
+                            push_id: req.body.push_id,
+                            push_categories: req.body.push_categories
+                        })
+                            .then(device => {
+                                res.send({
+                                    status: true,
+                                    device: Object.assign({}, device.toJSON(), {
+                                        user: undefined
+                                    })
+                                });
+                            })
+                            .catch(err => {
+                                res.send({
+                                    status: false,
+                                    message: err
+                                });
+                            });
+                    }
+                })
+                .catch(err => {
+                    res.send({
+                        status: false,
+                        message: err
+                    });
+                });
+        })(req, res, function() {
             Device.findOne({
                 push_id: req.body.push_id
             })
@@ -116,25 +86,49 @@ router.post(
                         device.user = req.user;
                         device.updateFields(fields);
 
-                        res.send({
-                            status: true,
-                            device: Object.assign({}, device.toJSON(), {
-                                user: device.user.getProfile()
+                        device.user
+                            .getProfile()
+                            .then(profile => {
+                                res.send({
+                                    status: true,
+                                    device: Object.assign({}, device.toJSON(), {
+                                        user: profile
+                                    })
+                                });
                             })
-                        });
+                            .catch(err => {
+                                res.status(500).send({
+                                    status: false,
+                                    message: err
+                                });
+                            });
                     } else {
                         Device.create({
                             user: req.user,
                             push_id: req.body.push_id,
-                            push_categories: req.body.push_categories || []
+                            push_categories: req.body.push_categories
                         })
                             .then(device => {
-                                res.send({
-                                    status: true,
-                                    device: Object.assign({}, device.toJSON(), {
-                                        user: device.user.getProfile()
+                                device.user
+                                    .getProfile()
+                                    .then(profile => {
+                                        res.send({
+                                            status: true,
+                                            device: Object.assign(
+                                                {},
+                                                device.toJSON(),
+                                                {
+                                                    user: profile
+                                                }
+                                            )
+                                        });
                                     })
-                                });
+                                    .catch(err => {
+                                        res.status(500).send({
+                                            status: false,
+                                            message: err
+                                        });
+                                    });
                             })
                             .catch(err => {
                                 res.send({
@@ -150,13 +144,13 @@ router.post(
                         message: err
                     });
                 });
-        } else {
-            res.status(400).send({
-                status: false,
-                message: Responses.MISSING_PARAMETERS
-            });
-        }
+        });
+    } else {
+        res.status(400).send({
+            status: false,
+            message: Responses.MISSING_PARAMETERS
+        });
     }
-);
+});
 
 module.exports = router;
