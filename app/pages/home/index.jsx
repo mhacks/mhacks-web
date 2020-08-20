4650c97a456b7c284b5e465767ef9438a910eb75
Carson Hoffman <c@rsonhoffman.com>
2020-08-20 14:47:59 -0400

    Add event calendar to home screen

--- app/pages/home/index.jsx
+++ app/pages/home/index.jsx
@@ -20,6 +20,18 @@ class HomePage extends React.Component {
         return (
             <BodyDiv>
                 <Landing />
+                <div style={{ textAlign: 'center', width: '100%' }}>
+                    <iframe
+                        src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;bgcolor=%230a192e&amp;ctz=America%2FNew_York&amp;src=Y19iYmg4bms2cDdoYjR2ZGF0NzU0bHB0cmxnc0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%233F51B5&amp;mode=WEEK&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showTitle=0&amp;showCalendars=0"
+                        style={{
+                            width: '80%',
+                            maxWidth: '800px',
+                            height: '600px'
+                        }}
+                        frameBorder={false}
+                        scrolling="no"
+                    ></iframe>
+                </div>
                 <Hero backgroundColor={this.props.theme.primary} />
                 <HackingCategories backgroundColor={this.props.theme.primary} />
                 <Faq backgroundColor={this.props.theme.primary} />
