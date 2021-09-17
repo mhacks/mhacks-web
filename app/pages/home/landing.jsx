dc0a1d25131c39faddcab1d235b1976790013f68
Robert Marzec <robertmarzecj@gmail.com>
2021-09-17 18:40:49 -0400

    updated the MLH logo to the 2022 version

--- app/pages/home/landing.jsx
+++ app/pages/home/landing.jsx
@@ -88,12 +88,12 @@ class Landing extends React.Component {
         return (
             <Wrapper>
                 <MLHBanner
-                    href="https://mlh.io/seasons/na-2021/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2021-season&utm_content=gray"
+                    href="https://mlh.io/seasons/2022/events"
                     target="_blank"
                 >
                     <MLHBannerImage
-                        src="https://s3.amazonaws.com/logged-assets/trust-badge/2021/mlh-trust-badge-2021-gray.svg"
-                        alt="Major League Hacking 2021 Hackathon Season"
+                        src="https://s3.amazonaws.com/logged-assets/trust-badge/2022/mlh-trust-badge-2022-gray.svg"
+                        alt="Major League Hacking 2022 Hackathon Season"
                     />
                 </MLHBanner>
                 <Container style={{ height: '100vh' }}>
