97bf292a030cd3051937f694fa2e129a74fd1001
Robert Marzec <robertmarzecj@gmail.com>
2021-09-16 21:29:28 -0400

    updated the MLH icon and link to the 2021 versions

--- app/pages/home/landing.jsx
+++ app/pages/home/landing.jsx
@@ -88,12 +88,12 @@ class Landing extends React.Component {
         return (
             <Wrapper>
                 <MLHBanner
-                    href="https://mlh.io/seasons/na-2020/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2020-season&utm_content=gray"
+                    href="https://mlh.io/seasons/na-2021/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2021-season&utm_content=gray"
                     target="_blank"
                 >
                     <MLHBannerImage
-                        src="https://s3.amazonaws.com/logged-assets/trust-badge/2020/mlh-trust-badge-2020-gray.svg"
-                        alt="Major League Hacking 2020 Hackathon Season"
+                        src="https://s3.amazonaws.com/logged-assets/trust-badge/2021/mlh-trust-badge-2021-gray.svg"
+                        alt="Major League Hacking 2021 Hackathon Season"
                     />
                 </MLHBanner>
                 <Container style={{ height: '100vh' }}>
