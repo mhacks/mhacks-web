ddae1d33faa421d1466a9e9aad6d73713f35044b
Zhixiang Teoh <47135468+zhixiangteoh@users.noreply.github.com>
2022-10-09 16:34:10 -0400

    Attempt to obscure mailgun keys

--- server/interactors/email.js
+++ server/interactors/email.js
@@ -1,110 +1,125 @@
-var Mailchimp = require('mailchimp-api-v3'),
-    config = require('../../config/default.js'),
-    Mandrill = require('mandrill-api/mandrill');
+var fs = require("fs");
+
+var Mailchimp = require("mailchimp-api-v3"),
+  config = require("../../config/default.js"),
+  Mandrill = require("mandrill-api/mandrill");
 
 var Errors = {
-    ALREADY_SUBSCRIBED: 'ALREADY_SUBSCRIBED',
-    MISSING_CONFIG: 'MISSING_CONFIG',
-    UNKNOWN: 'UNKNOWN_ERROR'
+  ALREADY_SUBSCRIBED: "ALREADY_SUBSCRIBED",
+  MISSING_CONFIG: "MISSING_CONFIG",
+  UNKNOWN: "UNKNOWN_ERROR",
 };
 
-const formData = require('form-data');
-const Mailgun = require('mailgun.js');
+const KEYS_FOLDER = "../../keys";
+
+const get_mailgun_api_key = () => {
+  const apikeyFolder = KEYS_FOLDER + "/mg_k";
+  const apikeyFiles = fs.readdirSync(apikeyFolder);
+  let apikey = "";
+  for (const apikeyFile of apikeyFiles) {
+    apikey += fs.readFileSync(apikeyFolder + "/" + apikeyFile, "utf8");
+  }
+  return apikey;
+};
+
+const get_mailgun_public_key = () => {
+  const pubkeyFolder = KEYS_FOLDER + "/mg_pk";
+  const pubkeyFiles = fs.readdirSync(pubkeyFolder);
+  let pubkey = "";
+  for (const pubkeyFile of pubkeyFiles) {
+    pubkey += fs.readFileSync(pubkeyFolder + "/" + pubkeyFile, "utf8");
+  }
+  return pubkey;
+};
+
+const formData = require("form-data");
+const Mailgun = require("mailgun.js");
 const mailgun = new Mailgun(formData);
 const mg = mailgun.client({
-  username: 'mhacks.org',
-  key: process.env.MAILGUN_API_KEY || 'key-3c4f7813f2541f7b7ac2774aa91a6b1c',
-  public_key: process.env.MAILGUN_PUBLIC_KEY || 'pubkey-e705b491e75108109796e42bdc6565a2'
+  username: "mhacks.org",
+  key: process.env.MAILGUN_API_KEY || get_mailgun_api_key(),
+  public_key: process.env.MAILGUN_PUBLIC_KEY || get_mailgun_public_key(),
 });
 
-
 function subscribe(email) {
-    return new Promise((resolve, reject) => {
-        if (!config.mailchimp_token || !config.mailchimp_listid) {
-            reject(Errors.MISSING_CONFIG);
-            return;
+  return new Promise((resolve, reject) => {
+    if (!config.mailchimp_token || !config.mailchimp_listid) {
+      reject(Errors.MISSING_CONFIG);
+      return;
+    }
+
+    var mailchimp = new Mailchimp(config.mailchimp_token);
+
+    mailchimp
+      .post({
+        path: "/lists/" + config.mailchimp_listid,
+        body: {
+          members: [
+            {
+              email_address: email,
+              status: "subscribed",
+            },
+          ],
+        },
+      })
+      .then((result) => {
+        // mailchimp-api-v3 module resolves on all 200 HTTP status codes
+        // so must check for errors here as well
+        if (result.errors.length > 0) {
+          var message = result.errors[0].error;
+          var error = null;
+
+          if (message.toLowerCase().indexOf("already a list member") != -1) {
+            error = Errors.ALREADY_SUBSCRIBED;
+          } else {
+            error = Errors.UNKNOWN;
+          }
+
+          reject(error);
+        } else {
+          resolve(result);
         }
+      })
+      .catch((err) => {
+        console.error(err);
 
-        var mailchimp = new Mailchimp(config.mailchimp_token);
-
-        mailchimp
-            .post({
-                path: '/lists/' + config.mailchimp_listid,
-                body: {
-                    members: [
-                        {
-                            email_address: email,
-                            status: 'subscribed'
-                        }
-                    ]
-                }
-            })
-            .then(result => {
-                // mailchimp-api-v3 module resolves on all 200 HTTP status codes
-                // so must check for errors here as well
-                if (result.errors.length > 0) {
-                    var message = result.errors[0].error;
-                    var error = null;
-
-                    if (
-                        message
-                            .toLowerCase()
-                            .indexOf('already a list member') != -1
-                    ) {
-                        error = Errors.ALREADY_SUBSCRIBED;
-                    } else {
-                        error = Errors.UNKNOWN;
-                    }
-
-                    reject(error);
-                } else {
-                    resolve(result);
-                }
-            })
-            .catch(err => {
-                console.error(err);
-
-                reject(Errors.UNKNOWN);
-            });
-    });
+        reject(Errors.UNKNOWN);
+      });
+  });
 }
 
 function sendEmailTemplate(
-    template_name,
-    template_content,
-    subject,
-    to_email,
-    from_email,
-    from_name
+  template_name,
+  template_content,
+  subject,
+  to_email,
+  from_email,
+  from_name
 ) {
-    return new Promise((resolve, reject) => {
-      var DOMAIN = 'mhacks.org';
-      var api_key = 'key-3c4f7813f2541f7b7ac2774aa91a6b1c';
-      var mailgun = require('mailgun-js')({ apiKey: api_key, domain: DOMAIN });
+  return new Promise((resolve, reject) => {
+    var DOMAIN = "mhacks.org";
+    var api_key = "key-3c4f7813f2541f7b7ac2774aa91a6b1c";
+    var mailgun = require("mailgun-js")({ apiKey: api_key, domain: DOMAIN });
 
-      console.log(template_content);
+    console.log(template_content);
 
-      var data = {
-        from: from_email,
-        to: to_email,
-        subject: subject,
-        html: template_content,
-      };
+    var data = {
+      from: from_email,
+      to: to_email,
+      subject: subject,
+      html: template_content,
+    };
 
-      mailgun.messages().send(data, function (error, body) {
-        console.log(body);
-        if(error)
-        {
-          reject(error);
-        }
-        else
-        {
-          resolve(body);
-        }
-      });
+    mailgun.messages().send(data, function(error, body) {
+      console.log(body);
+      if (error) {
+        reject(error);
+      } else {
+        resolve(body);
+      }
+    });
 
-
-      /*
+    /*
       mg.messages.create('mhacks.org', {
           from: from_email,
           to: to_email,
@@ -123,7 +138,7 @@ function sendEmailTemplate(
           mg.messages.sendTemplate
           */
 
-        /*
+    /*
         if (!config.mandrill_token) {
             reject(Errors.MISSING_CONFIG);
             return;
@@ -162,11 +177,11 @@ function sendEmailTemplate(
             }
         );
         */
-    });
+  });
 }
 
 module.exports = {
-    subscribe,
-    sendEmailTemplate,
-    Errors
+  subscribe,
+  sendEmailTemplate,
+  Errors,
 };
